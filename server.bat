f131fc20192d04eee9ca640da8d9bff9c8d07aa0
OCEAN OF ANYTHING <oceanofanything@gmail.com>
2022-06-08 21:01:56 +0530

    update

--- server.bat
+++ server.bat
@@ -4,11 +4,11 @@ set loop=0
 git add .
 git commit -m "update"
 :loop
-set datetimef=%date:~3,2%%date:~-4%%time:~0,2%%time:~3,2%%time:~6,2%%random%
+set datetimef=%date:~0,2%_%date:~3,2%_%date:~-4%__%time:~0,2%_%time:~3,2%_%time:~6,2%__%random%
 set datetimef=%datetimef: =%
-%datetimef% > data.txt
+echo %datetimef% > data.txt
 git add data.txt
-git commit -m "%datetimef%"
+git commit -m %datetimef%
 
 set /a loop=%loop%+1 
 if "%loop%"=="500" goto finish
