3f7008c4738ac1fa2d7c8d1a8b3e61ab50db7c52
OCEAN OF ANYTHING <oceanofanything@gmail.com>
2022-06-08 21:15:46 +0530

    update

--- server.bat
+++ server.bat
@@ -1,19 +1,26 @@
 @echo off
 set loop=0
+set maxLoop = 10
 
-git add .
-git commit -m "update"
+git add . > nul
+git commit -m "update" > nul
 :loop
 set datetimef=%date:~0,2%_%date:~3,2%_%date:~-4%__%time:~0,2%_%time:~3,2%_%time:~6,2%__%random%
 set datetimef=%datetimef: =%
-echo %datetimef% > data.txt >nul
+echo %datetimef% > data.txt > nul
 git add data.txt >nul
-git commit -m %datetimef% >nul
+git commit -m %datetimef% > nul
 set /a loop=%loop%+1 
 echo [%loop%] Commit Created At: %datetimef%
-if "%loop%"=="10" goto finish
+if "%loop%"==%maxLoop% goto commit
 goto loop
 
-:finish
-git push -u origin main
-echo It's Done
+:commit
+echo Commiting All Changes...
+git push -u origin main >nul
+echo x=msgbox("Succesfully Commited %maxLoop% Contributes !!!",, "Success !!!") > success.vbs > nul
+cscript //Nologo success.vbs > nul
+echo .
+echo .
+echo Press Any Key To Exit...
+pause >nul
